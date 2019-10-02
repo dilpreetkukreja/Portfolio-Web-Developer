72590949f20b322d9a08be4fdb43e8122b54e82b
dilpreet kaur <dilpreet.kukreja@gmail.com>
2019-10-02 00:14:01 -0700

    adding smooth scroll

--- js/script.js
+++ js/script.js
@@ -4,4 +4,14 @@ $(function() {
             $('.navbar-toggler').trigger( "click" );
         }
     });
+    /* adding smooth scrolling through the website*/
+    $('nav .nav-link').click(function(e){
+    	e.preventDefault();
+    	console.log($(this.hash));//section which clicked element referening to
+    	//animate function require jquery-3.4.1.min.js. It will not work using jquery-3.3.1.slim.min.js
+    	$('body, html').animate({	
+    		//set scrollbar position from top = section distance from top
+    		scrollTop: $(this.hash).offset().top
+    	}, 500); //1 sec duration of scroll
+    })
 });
